cd7e77e0aa37e28b57723a0377a5ab9873e24009
Samson Qian <samsonqian@gmail.com>
2020-11-11 17:27:49 -0800

    refactor

--- src/main.py
+++ src/main.py
@@ -1,12 +1,15 @@
 import sys
-
-import mlflow
-import mlflow.keras
+import os
 
 import numpy as np
 import pandas as pd
 import random
+import argparse
 
+import mlflow
+import mlflow.keras
+
+import tensorflow as tf
 import keras
 from keras import backend as K
 from keras.models import Sequential
@@ -15,6 +18,9 @@ from keras.optimizers import RMSprop
 
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.model_selection import train_test_split
+
+import warnings
+warnings.filterwarnings("ignore")
 
 
 def binary_model(dim):
@@ -71,7 +77,7 @@ def f1(y_true, y_pred):
     return 2 * ((precision_val * recall_val)/(precision_val + recall_val + K.epsilon()))
 
 
-def train_and_log(mdl, train_x, train_y, valid_x, valid_y, p1, p2, p3):
+def train_and_log(mdl, train_x, train_y, valid_x, valid_y, learning_rate, batch_size, epochs):
     """
     Train and evaluate the model based on training and validation data and outputs the accuracy,
     recall, precision, and f1 score at each epoch. Logs inputted hyperparameters and metrics
@@ -81,22 +87,22 @@ def train_and_log(mdl, train_x, train_y, valid_x, valid_y, p1, p2, p3):
     :param train_y: training labels
     :param valid_x: validation features
     :param valid_y: validation labels
-    :param p1: hyperparameter 1 (learning rate)
-    :param p2: hyperparameter 2 (batch size)
-    :param p3: hyperparameter 3 (epochs)
+    :param learning_rate: gradient descent learning rate
+    :param batch_size: training batch size
+    :param epochs: number of epochs
     :return: history of metrics from training
     """
-    mdl.compile(optimizer=RMSprop(lr=p1),
+    mdl.compile(optimizer=RMSprop(lr=learning_rate),
                 loss="binary_crossentropy",
                 metrics=["accuracy", recall, precision, f1])
 
     with mlflow.start_run():
-        hist = mdl.fit(train_x, train_y, batch_size=p2, epochs=p3,
+        hist = mdl.fit(train_x, train_y, batch_size=batch_size, epochs=epochs,
                        verbose=1, validation_data=(valid_x, valid_y))
-        mlflow.log_param("learning_rate", p1)
-        mlflow.log_param("batch_size", p2)
-        mlflow.log_param("epochs", p3)
-        metrics = ["accuracy", "recall", "precision", "f1", "val_accuracy", "val_recall", "val_precision", "val_f1"]
+        mlflow.log_param("learning_rate", learning_rate)
+        mlflow.log_param("batch_size", batch_size)
+        mlflow.log_param("epochs", epochs)
+        metrics = ["acc", "recall", "precision", "f1", "val_acc", "val_recall", "val_precision", "val_f1"]
         for metric in metrics:
             for step in range(len(hist.history[metric])):
                 mlflow.log_metric(metric, hist.history[metric][step], step=step+1)
@@ -138,23 +144,39 @@ def preprocess(df):
     return train_x.to_numpy(), valid_x.to_numpy(), train_y.to_numpy(), valid_y.to_numpy()
 
 
+def parse_args():
+    """
+    Get arguments to run detection model. Input video file to detect social distancing, or
+    by default, use webcam for detection.
+    :return: program arguments
+    """
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-lr", "--learning_rate", type=float, default="0.0001",
+                    help="model learning rate")
+    ap.add_argument("-b", "--batch_size", type=int, default="64",
+                    help="training batch size")
+    ap.add_argument("-e", "--epochs", type=int, default="50",
+                    help="training epochs")
+    return vars(ap.parse_args())
+
+
 if __name__ == "__main__":
 
-    data = pd.read_csv("data/titanic.csv")
+    data = pd.read_csv("../data/titanic.csv")
     training_experiments = 20
+    args = parse_args()
 
     x_train, x_validation, y_train, y_validation = preprocess(data)
 
-    for iteration in range(1, training_experiments+1):
-        print("Training iteration: " + str(iteration))
-        #P1 = random.uniform(0.000001, 0.1)   learning rate
-        #P2 = random.randint(10, 500)   batch size
-        #P3 = random.randint(10, 50)     epochs
-        P1 = sys.argv[1]
-        P2 = sys.argv[2]
-        P3 = sys.argv[3]
+    for iteration in range(training_experiments):
+        print("Training experiment: " + str(iteration+1))
+
+        LEARNING_RATE = args["learning_rate"]
+        BATCH_SIZE = args["batch_size"]
+        EPOCHS = args["epochs"]
 
         model = binary_model(x_train.shape[1])
-        history = train_and_log(model, x_train, y_train, x_validation, y_validation, P1, P2, P3)
+        history = train_and_log(model, x_train, y_train, x_validation, y_validation,
+                                LEARNING_RATE, BATCH_SIZE, EPOCHS)
         print(history)
-        print("Training iteration " + str(iteration) + " completed.")
+        print("Training experiment " + str(iteration+1) + " completed.")
